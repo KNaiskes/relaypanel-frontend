78ba0ddb267c2b5c9e62b79405359b38d06ea411
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-12-01 22:21:49 +0200

    Add GetSingleRelay function in service

--- src/components/service/relay/relay.js
+++ src/components/service/relay/relay.js
@@ -1,9 +1,16 @@
-const API_URL = 'http://localhost:8000/api/relays/';
+const BASE_URL = 'http://localhost:8000/api/relays/';
 
 let headers = {"Content-Type": "application/json"};
 headers["Authorization"] = `Token c85e473c1cb01230fc4f11b7afc07e1388d195eb`;
 
 export function GetRelayList() {
-    return fetch(API_URL, {headers,})
+    return fetch(BASE_URL, {headers,})
 	.then(data => data.json())
+}
+
+export function GetSingleRelay(id) {
+    const url = `${BASE_URL}${id}`;
+    return fetch(url, {headers,})
+	.then(data => data.json())
+    console.log(url)
 }
