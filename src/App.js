8b763628a0620da28887304538d84734ac8a73d3
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-12-01 13:36:05 +0200

    Make RelayList an independent component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
 
-import { GetRelayList } from './components/service/relay/relay';
+
+import { RelayList } from './components/RelayList';
 
 function App() {
-    const [list, setList] = useState([]);
-    useEffect(() => {
-   let mounted = true;
-   GetRelayList()
-     .then(items => {
-       if(mounted) {
-         setList(items)
-       }
-     })
-   return () => mounted = false;
-    }, [])
-    console.log(list);
     return (
-	<div className="App">
-	    <ul>
-		{list.map(item => <li key={item.name}>{item.name}</li>)}
-	    </ul>
-	</div>
+	<RelayList/>
     );
 }
 
---
+++ src/components/RelayList.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+import { GetRelayList } from './service/relay/relay';
+
+export function RelayList() {
+    const [list, setList] = useState([]);
+    useEffect(() => {
+	let mounted = true;
+	GetRelayList()
+	    .then(items => {
+		if(mounted) {
+		    setList(items)
+		}
+	    })
+	return () => mounted = false;
+    }, [])
+
+    return (
+	<div className="App">
+	    <ul>
+		{list.map(item => <li key={item.name}>{item.name}</li>)}
+	    </ul>
+	</div>
+    );
+}
