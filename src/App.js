4c5bf232c468bf805de9db034cb74dfaaf428482
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-12-02 20:49:30 +0200

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { RelayList } from './components/RelayList';
 import { SingleRelay } from './components/SingleRelay';
 
+import { Navbar } from './components/Navbar';
+
 function App() {
     return (
 	    <div className="App">
+	    <Navbar />
 	    <BrowserRouter>
 	    <Switch>
 	    <Route path='/' exact component={RelayList} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Navbar() {
+    return (
+	<div className="navbar">
+	    <ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="#">#</a></li>
+	    </ul>
+	</div>
+    );
+}
