2ee8beba9cec88c43c24bb7c2d1377c9a56bed8f
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-12-02 19:55:32 +0200

    Add router Switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { RelayList } from './components/RelayList';
 import { SingleRelay } from './components/SingleRelay';
@@ -9,7 +9,10 @@ function App() {
     return (
 	    <div className="App">
 	    <BrowserRouter>
+	    <Switch>
+	    <Route path='/' exact component={RelayList} />
 	    <Route path='/relays/:id' exact component={SingleRelay} />
+	    </Switch>
 	    </BrowserRouter>
 	    </div>
     );
