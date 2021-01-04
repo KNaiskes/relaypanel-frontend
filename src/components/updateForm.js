68dc14e0630caf644dd8eca01ecf86dbff1cd9f4
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-04 19:36:28 +0200

    Fix indentation

--- src/components/updateForm.js
+++ src/components/updateForm.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-export function UpdateForm() {
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = data => {
-    console.log(data);
-  };
+export function UpdateForm(relayId) {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => {
+	console.log(data);
+    };
 
-  console.log(watch("example"));
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>Update Name</label>
-	<input
-	    name="updateName"
-	    defaultValue="##"
-	    ref={register({ required: true, maxLength: 10})}
-	/>
-      <label>Update device type</label>
-      <input
-          name="updateDevice"
-	  defaultValue="##"
-        ref={register({ required: true, maxLength: 10 })}
-      />
-      {errors.exampleRequired && <p>This field is required</p>}
-      <input type="submit" />
-    </form>
-  );
+    console.log(watch("example"));
+
+    return (
+	<form onSubmit={handleSubmit(onSubmit)}>
+	    <label>Update Name</label>
+	    <input
+		name="updateName"
+		defaultValue="##"
+		ref={register({ required: true, maxLength: 10})}
+	    />
+	    <label>Update device type</label>
+	    <input
+		name="updateDevice"
+		defaultValue="##"
+		ref={register({ required: true, maxLength: 10 })}
+	    />
+	    {errors.exampleRequired && <p>This field is required</p>}
+	    <input type="submit" />
+	</form>
+    );
 }
