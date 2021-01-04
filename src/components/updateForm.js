2e31d142e37232d64794a25be650a751f7905976
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-04 21:10:24 +0200

    Get relay's data from url (id)

--- src/components/updateForm.js
+++ src/components/updateForm.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
+import { useParams } from 'react-router-dom';
 
-export function UpdateForm(relayId) {
+import { GetSingleRelay } from './service/relay/relay';
+
+export function UpdateForm() {
+    const { id } = useParams();
+
+    const [relay, setRelay] = useState([]);
+    useEffect(() => {
+	let mounted = true;
+	GetSingleRelay(id)
+	    .then(data => {
+		if(mounted) {
+		    setRelay(data)
+		}
+	    })
+	return () => mounted = false;
+    }, [id])
+
+
     const { register, handleSubmit, watch, errors } = useForm();
     const onSubmit = data => {
 	console.log(data);
     };
 
-
     console.log(watch("example"));
+    console.log("Relay: ", relay);
 
     return (
 	<form onSubmit={handleSubmit(onSubmit)}>
