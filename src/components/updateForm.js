c6bbf9eb63715794dd69212fa26ed617ff970018
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-01-05 20:36:56 +0200

    Update name and device when form is submited

--- src/components/updateForm.js
+++ src/components/updateForm.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
 import { useParams } from 'react-router-dom';
 
-import { GetSingleRelay } from './service/relay/relay';
+import { GetSingleRelay, UpdateRelay } from './service/relay/relay';
 
 export function UpdateForm() {
     const { id } = useParams();
@@ -23,23 +23,22 @@ export function UpdateForm() {
     const { register, handleSubmit, watch, errors } = useForm();
     const onSubmit = data => {
 	console.log(data);
+	relay.name = data.updateName;
+	relay.device = data.updateDevice;
+	//console.log("Relay: ", relay);
+	UpdateRelay(id, relay)
     };
-
-    console.log(watch("example"));
-    console.log("Relay: ", relay);
 
     return (
 	<form onSubmit={handleSubmit(onSubmit)}>
 	    <label>Update Name</label>
 	    <input
 		name="updateName"
-		defaultValue="##"
 		ref={register({ required: true, maxLength: 10})}
 	    />
 	    <label>Update device type</label>
 	    <input
 		name="updateDevice"
-		defaultValue="##"
 		ref={register({ required: true, maxLength: 10 })}
 	    />
 	    {errors.exampleRequired && <p>This field is required</p>}
